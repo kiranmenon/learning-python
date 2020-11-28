84463664ebd21233770869ebafe740ba8ef5682f
kiranmenon <targetkiranp@gmail.com>
2020-11-28 15:36:11 +0530

    print fn formattings

---
+++ basics/formatting/different_formattings.py
@@ -0,0 +1,8 @@
+
+print("This is an int : %d and this a float %0.4f" %(12, 5.05))
+print("This is an int : {0:d} and this a float {1:0.4f}".format(12,5.05))
+data = { "data1" :12, "data2" : 5.05 }
+print("This is an int : {0[data1]:d} and this a float {0[data2]:0.4f}".format(data))
+
+
+
